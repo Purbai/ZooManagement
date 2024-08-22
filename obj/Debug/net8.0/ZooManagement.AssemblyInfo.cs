9968556b03aff730a4cb711dec269717856379fc
anum1608 <riaz.anum@outlook.com>
2024-08-22 12:45:36 +0100

    added gitignore

--- obj/Debug/net8.0/ZooManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/ZooManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c622349e818537598ee8824b6d15c2594ffbd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83b767244e7ef00e3b47ea62ee873643727dde1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
